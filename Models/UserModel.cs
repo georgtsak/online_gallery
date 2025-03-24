86958ea21d9ebfd587d55d4354ea01007a9abdfd
yorgos <georgetsakiris21@gmail.com>
2025-03-24 18:34:13 +0200

    added UserModel

--- Models/UserModel.cs
+++ Models/UserModel.cs
@@ -1,17 +1,28 @@
 ﻿using System.ComponentModel.DataAnnotations;
+using System.Transactions;
 
 namespace OnlineGallery.Models
 {
-    public class UserModel
+    public class User
     {
         public int Id { get; set; }
-        public string Firstname { get; set; } = "";
-        public string Lastname { get; set; } = "";
+        public string FullName { get; set; }
+
         [DataType(DataType.EmailAddress)]
-        public string Email { get; set; } = "";
+        public string Email { get; set; }
+
         [DataType(DataType.Password)]
-        public string Password { get; set; } = "";
-        public string Salt { get; set; } = "";
-        public int Role { get; set; } = 0;
+        public string Password { get; set; }
+
+        public string Salt { get; set; } // hashing kwdikou
+
+        public string Role { get; set; } // artist or buyer
+
+        public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+
+        public ICollection<ArtworkModel> Artworks { get; set; } // artist
+        public ICollection<TransactionModel> Transactions { get; set; } // buyer
     }
+
+
 }
