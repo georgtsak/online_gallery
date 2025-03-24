dfa81f3a0a0191539f3c0cd818f36bb1cde3935c
yorgos <georgetsakiris21@gmail.com>
2025-03-24 18:35:59 +0200

    added TransactionModel

--- Models/TransactionModel.cs
+++ Models/TransactionModel.cs
@@ -1,1 +1,14 @@
-﻿
+﻿namespace OnlineGallery.Models
+{
+    public class TransactionModel
+    {
+        public int Id { get; set; }
+        public int BuyerId { get; set; }
+        public User Buyer { get; set; }
+        public int ArtworkId { get; set; }
+        public ArtworkModel Artwork { get; set; }
+        public decimal Price { get; set; }
+        public DateTime PurchasedAt { get; set; } = DateTime.UtcNow;
+    }
+
+}
