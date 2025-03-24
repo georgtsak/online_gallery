866d9060c5901c4239274773b93386afe2114f5d
yorgos <georgetsakiris21@gmail.com>
2025-03-24 18:34:54 +0200

    added ArtworkModel

--- Models/ArtworkModel.cs
+++ Models/ArtworkModel.cs
@@ -1,1 +1,16 @@
-﻿
+﻿namespace OnlineGallery.Models
+{
+    public class ArtworkModel
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public decimal Price { get; set; }
+        public string ImageUrl { get; set; }
+        public int ArtistId { get; set; }
+        public User Artist { get; set; }
+        public string Status { get; set; } = "Available"; // default timh
+        public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+    }
+
+}
