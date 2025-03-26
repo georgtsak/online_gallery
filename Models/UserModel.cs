b92daf70f5d4dbf5a25812e4b41e7013351270a1
yorgos <georgetsakiris21@gmail.com>
2025-03-26 17:33:19 +0200

    updated user model

--- Models/UserModel.cs
+++ Models/UserModel.cs
@@ -3,26 +3,43 @@ using System.Transactions;
 
 namespace OnlineGallery.Models
 {
-    public class UserModel
-    {
-        public int Id { get; set; }
-        public string FullName { get; set; }
+	public class UserModel
+	{
+		public int Id { get; set; }
+		public string FullName { get; set; }
 
-        [DataType(DataType.EmailAddress)]
-        public string Email { get; set; }
+		[DataType(DataType.EmailAddress)]
+		public string Email { get; set; }
 
-        [DataType(DataType.Password)]
-        public string Password { get; set; }
+		[DataType(DataType.Password)]
+		public string Password { get; set; }
 
-        public string Salt { get; set; } // hashing kwdikou
+		public string Salt { get; set; } //hashing
 
-        public string Role { get; set; } // artist or buyer
+		public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
 
-        public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+		public ICollection<ArtworkModel> Artworks { get; set; } = new List<ArtworkModel>(); // erga texnhs
+		public ICollection<TransactionModel> Transactions { get; set; } = new List<TransactionModel>(); // agores
 
-        public ICollection<ArtworkModel> Artworks { get; set; } // artist
-        public ICollection<TransactionModel> Transactions { get; set; } // buyer
-    }
+		public string Role
+		{
+			get
+			{
+				bool isArtist = Artworks.Any();
+				bool isBuyer = Transactions.Any();
+
+				if (isArtist && isBuyer)
+					return "Artist & Buyer";
+				else if (isArtist)
+					return "Artist";
+				else if (isBuyer)
+					return "Buyer";
+				else
+					return "Visitor"; // den exei kanei akomh kamia energeia
+			}
+		}
+	}
+
 
 
 }
