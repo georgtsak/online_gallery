20c65fc61f0fa7aeafcdc5290d58103054c0f140
yorgos <georgetsakiris21@gmail.com>
2025-03-26 18:36:21 +0200

    added db + migration

---
+++ Data/ApplicationDbContext.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.EntityFrameworkCore;
+using OnlineGallery.Models;
+using System.Collections.Generic;
+using System.Reflection.Emit;
+
+namespace OnlineGallery.Data
+{
+	public class ApplicationDbContext : DbContext
+	{
+		public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+			: base(options) { }
+
+		public DbSet<UserModel> Users { get; set; }
+		public DbSet<ArtworkModel> Artworks { get; set; }
+		public DbSet<TransactionModel> Transactions { get; set; }
+
+		protected override void OnModelCreating(ModelBuilder modelBuilder)
+		{
+			modelBuilder.Entity<TransactionModel>()
+				.HasOne(t => t.Artwork)
+				.WithMany()
+				.HasForeignKey(t => t.ArtworkId)
+				.OnDelete(DeleteBehavior.Cascade);
+
+			modelBuilder.Entity<TransactionModel>()
+				.HasOne(t => t.Buyer)
+				.WithMany()
+				.HasForeignKey(t => t.BuyerId)
+				.OnDelete(DeleteBehavior.NoAction);
+		}
+
+
+	}
+}
--- Program.cs
+++ Program.cs
@@ -1,4 +1,11 @@
+using Microsoft.EntityFrameworkCore;
+using OnlineGallery.Data;
+
 var builder = WebApplication.CreateBuilder(args);
+
+// Προσθήκη της βάσης δεδομένων (SQL Server ή SQLite)
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+	options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
@@ -8,16 +15,18 @@ var app = builder.Build();
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
-    app.UseExceptionHandler("/Home/Error");
+	app.UseExceptionHandler("/Home/Error");
 }
+
 app.UseStaticFiles();
 
 app.UseRouting();
 
+app.UseAuthentication(); // Αν προσθέσεις authentication
 app.UseAuthorization();
 
 app.MapControllerRoute(
-    name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}");
+	name: "default",
+	pattern: "{controller=Home}/{action=Index}/{id?}");
 
 app.Run();
