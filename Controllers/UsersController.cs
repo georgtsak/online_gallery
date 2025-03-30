766c7b81a3de9aa04c354cec3807e6a2801e5989
yorgos <georgetsakiris21@gmail.com>
2025-03-30 16:40:46 +0300

    implemented user login functionality

--- Controllers/UsersController.cs
+++ Controllers/UsersController.cs
@@ -18,12 +18,25 @@ public class UsersController : Controller
 		_context = context;
 	}
 
+	// **************************************************** return View ******
 	// GET: Users/Create
 	public IActionResult Create()
 	{
 		return View();
 	}
 
+	// GET: Users/Register
+	public IActionResult Register()
+	{
+		return View();
+	}
+	public IActionResult Login()
+	{
+		//Debug.WriteLine("login page opened");
+		return View();
+	}
+
+	// ********************************************************* create ******
 	// POST: Users/Create
 	[HttpPost]
 	[ValidateAntiForgeryToken]
@@ -45,14 +58,9 @@ public class UsersController : Controller
 		return View(user);
 	}
 
-	// GET: Users/Register
-	public IActionResult Register()
-	{
-		return View();
-	}
-
-    // POST: Users/Register
-    [HttpPost]
+	// ******************************************************* register ******
+	// POST: Users/Register
+	[HttpPost]
     public async Task<IActionResult> Register(User user)
     {
         var salt = GenerateSalt();
@@ -93,10 +101,38 @@ public class UsersController : Controller
         return View(user);
     }
 
+	// ********************************************************** login ******
+	// POST: Users/Login
+	[HttpPost]
+	public async Task<IActionResult> Login(string email, string password)
+	{
+		var user = await _context.Users.FirstOrDefaultAsync(u => u.Email == email);
 
+		if (user == null)
+		{
+			ModelState.AddModelError("Email", "Το email δεν βρέθηκε.");
+			return View();
+		}
 
+		var hashedPassword = HashPassword(password, user.Salt);
 
-    private string GenerateSalt()
+		if (user.Password != hashedPassword)
+		{
+			ModelState.AddModelError("Password", "Λάθος κωδικός.");
+			return View();
+		}
+
+		/* session
+		HttpContext.Session.SetString("Id", user.Id.ToString());
+		HttpContext.Session.SetString("Role", user.Role.ToString());*/
+
+		Debug.WriteLine($"User {user.Email} logged in!");
+
+		return RedirectToAction("Index", "Home");
+	}
+
+	// ******************************************************** hashing ******
+	private string GenerateSalt()
 	{
 		var saltBytes = new byte[16];
 		using (var rng = new RNGCryptoServiceProvider())
@@ -115,118 +151,4 @@ public class UsersController : Controller
 			return Convert.ToBase64String(hashedBytes);
 		}
 	}
-
-	// GET: Users/Details/5
-	public async Task<IActionResult> Details(int? id)
-	{
-		if (id == null)
-		{
-			return NotFound();
-		}
-
-		var user = await _context.Users
-			.FirstOrDefaultAsync(m => m.Id == id);
-		if (user == null)
-		{
-			return NotFound();
-		}
-
-		return View(user);
-	}
-
-	// GET: Users/Edit/5
-	public async Task<IActionResult> Edit(int? id)
-	{
-		if (id == null)
-		{
-			return NotFound();
-		}
-
-		var user = await _context.Users.FindAsync(id);
-		if (user == null)
-		{
-			return NotFound();
-		}
-		return View(user);
-	}
-
-	// POST: Users/Edit/5
-	[HttpPost]
-	[ValidateAntiForgeryToken]
-	public async Task<IActionResult> Edit(int id, [Bind("Id,FullName,Email,Password,Role")] User user)
-	{
-		if (id != user.Id)
-		{
-			return NotFound();
-		}
-
-		if (ModelState.IsValid)
-		{
-			try
-			{
-				// Αν θέλουμε να ενημερώσουμε το password, πρέπει να γίνει hashing
-				if (!string.IsNullOrEmpty(user.Password))
-				{
-					var salt = GenerateSalt();
-					user.Password = HashPassword(user.Password, salt);
-					user.Salt = salt;
-				}
-
-				_context.Update(user);
-				await _context.SaveChangesAsync();
-			}
-			catch (DbUpdateConcurrencyException)
-			{
-				if (!UserExists(user.Id))
-				{
-					return NotFound();
-				}
-				else
-				{
-					throw;
-				}
-			}
-			return RedirectToAction(nameof(Index));
-		}
-		return View(user);
-	}
-
-	// GET: Users/Delete/5
-	public async Task<IActionResult> Delete(int? id)
-	{
-		if (id == null)
-		{
-			return NotFound();
-		}
-
-		var user = await _context.Users
-			.FirstOrDefaultAsync(m => m.Id == id);
-		if (user == null)
-		{
-			return NotFound();
-		}
-
-		return View(user);
-	}
-
-	// POST: Users/Delete/5
-	[HttpPost, ActionName("Delete")]
-	[ValidateAntiForgeryToken]
-	public async Task<IActionResult> DeleteConfirmed(int id)
-	{
-		var user = await _context.Users.FindAsync(id);
-		_context.Users.Remove(user);
-		await _context.SaveChangesAsync();
-		return RedirectToAction(nameof(Index));
-	}
-
-	private bool UserExists(int id)
-	{
-		return _context.Users.Any(e => e.Id == id);
-	}
-    public IActionResult Login()
-    {
-        Debug.WriteLine("login page opened");
-        return View();
-    }
 }
