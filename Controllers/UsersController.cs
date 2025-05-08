e31cf42894721b9b670a70aed86a68823eae5ed5
yorgos <georgetsakiris21@gmail.com>
2025-05-08 17:33:39 +0300

    modify alert UI

--- Controllers/UsersController.cs
+++ Controllers/UsersController.cs
@@ -251,7 +251,7 @@ public class UsersController : Controller
 
         await _context.SaveChangesAsync();
 
-        TempData["SuccessMsg"] = "Password changed successfully!";
+        TempData["InfoMsg"] = "Password changed successfully!";
         return RedirectToAction("Profile", new { section = "account" });
     }
 
@@ -275,7 +275,7 @@ public class UsersController : Controller
         }
 
         TempData["ConfirmDelete"] = true;
-        TempData["SuccessMsg"] = "Account deletion will log you out and return you to the homepage.";
+        TempData["InfoMsg"] = "Account deletion will log you out and return you to the homepage.";
         return RedirectToAction("Profile", new { section = "account" });
     }
 
@@ -321,7 +321,7 @@ public class UsersController : Controller
 
         await _context.SaveChangesAsync();
 
-        TempData["SuccessMsg"] = "Full Name changed successfully!";
+        TempData["InfoMsg"] = "Full Name changed successfully!";
         return RedirectToAction("Profile", new { section = "account" });
     }
 }
