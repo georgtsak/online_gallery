a9cc111b5e98f01613384209af45abfb91ec0342
yorgos <georgetsakiris21@gmail.com>
2025-05-13 16:13:23 +0300

    show top 5 artists based on sales (homepage)

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,22 +1,63 @@
 using OnlineGallery.Models;
 using Microsoft.AspNetCore.Mvc;
 using System.Diagnostics;
+using Microsoft.EntityFrameworkCore;
+using System.Security.Claims;
+using OnlineGallery.Data;
 
 namespace OnlineGallery.Controllers
 {
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        private readonly AppDbContext _context;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ILogger<HomeController> logger, AppDbContext context)
         {
             _logger = logger;
+            _context = context;
         }
 
         public IActionResult Index()
         {
-            return View();
+            var topArtists = _context.Transactions
+                .Where(t => t.Status == TransactionStatus.Completed)
+                .Include(t => t.Artwork)
+                .ThenInclude(a => a.Artist)
+                .GroupBy(t => t.Artwork.ArtistId)
+                .Select(g => new
+                {
+                    Artist = g.First().Artwork.Artist,
+                    ArtistId = g.Key,
+                    SalesCount = g.Count()
+                })
+                .OrderByDescending(x => x.SalesCount)
+                .Take(5)
+                .ToList()
+                .Select(x =>
+                {
+                    var sampleArtwork = _context.Artworks
+                        .Where(a => a.ArtistId == x.ArtistId)
+                        .OrderBy(r => Guid.NewGuid())
+                        .FirstOrDefault();
+
+                    return new HomeModel.TopArtists1
+                    {
+                        Artist = x.Artist,
+                        SalesCount = x.SalesCount,
+                        SampleArtwork = sampleArtwork
+                    };
+                })
+                .ToList();
+
+            var model = new HomeModel
+            {
+                TopArtists = topArtists
+            };
+
+            return View(model);
         }
+
 
         public IActionResult Privacy()
         {
---
+++ Models/HomeModel.cs
@@ -0,0 +1,20 @@
+﻿namespace OnlineGallery.Models
+{
+    public class HomeModel
+    {
+        public List<ArtworksModel> RecentArtworks { get; set; }
+
+        public List<TopArtists1> TopArtists { get; set; }
+
+        public List<ArtworksModel> RecommendedArtworks { get; set; }
+
+        public class TopArtists1
+        {
+            public User Artist { get; set; }                      
+            public ArtworksModel SampleArtwork { get; set; } // random artwork
+            public int SalesCount { get; set; }     
+        }
+        
+    }
+
+}
